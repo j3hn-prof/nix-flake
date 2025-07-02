fea8a5beea29ca708a5e42d170605a4e06335a16
John Rossi <johnrossi2601@gmail.com>
2025-07-01 21:31:29 -0400

    created and filled out basic configuration for `nibble`

---
+++ hosts/nibble/configuration.nix
@@ -0,0 +1,112 @@
+{
+  inputs,
+  pkgs,
+  lib,
+  host,
+  user,
+  dotfiles,
+  modules,
+  system,
+  stateVersion,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+    "${modules}" # Custom application module collection
+  ];
+
+  # desktops.gnome.enable = true;
+  desktops.niri.enable = true;
+
+  shells.fish.enable = true;
+
+  # social.discord.enable = true;
+
+  # gaming.minecraft.enable = true;
+  
+  widgets.waybar.enable = true;
+
+  terminals.ghostty.enable = true;
+
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  networking = {
+    hostName = host;
+    networkmanager.enable = true;
+  };
+
+  time.timeZone = "America/New_York";
+
+  # Set default locale and additional locale environment variables
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = builtins.listToAttrs (map (lc: {
+      name = lc; value = "en_US.UTF-8";
+    }) [
+      "LC_ADDRESS" "LC_IDENTIFICATION" "LC_MEASUREMENT" "LC_MONETARY"
+      "LC_NAME" "LC_NUMERIC" "LC_PAPER" "LC_TELEPHONE" "LC_TIME"
+    ]);
+  };
+
+  # Enable CUPS printing service
+  services.printing.enable = true;
+
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  users.users.${user} = {
+    isNormalUser = true;
+    description = "John Rossi";
+    extraGroups = [ "networkmanager" "wheel" ]; # Network and sudo privileges
+    ignoreShellProgramCheck = true;
+  };
+
+  # Configure sudo to extend authentication timeout to 1 hour
+  security.sudo = {
+    enable = true;
+    extraConfig = ''
+      Defaults timestamp_timeout=60
+    '';
+  };
+
+  # List of packages installed system-wide
+  environment.systemPackages = with pkgs; [
+    neovim
+    btop
+    git
+    fastfetch
+    tree
+    nixfmt-rfc-style
+    yazi
+    vesktop
+  ];
+  
+  fonts.packages = with pkgs; [
+    inter
+    nerd-fonts.adwaita-mono
+    nerd-fonts.symbols-only
+    noto-fonts-cjk-sans
+    cantarell-fonts
+    inter
+  ];
+
+  environment.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  # Set the initial system state version (required for upgrades)
+  system.stateVersion = stateVersion;
+
+  # Enable experimental Nix features: flakes and nix-command
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+}
--- modules/gaming/steam.nix
+++ modules/gaming/steam.nix
@@ -1,28 +1,31 @@
-{ config, pkgs, lib, ...}:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
-	options = {
+  options.gaming.steam.enable = lib.mkEnableOption "Enable the `Steam` store-front, launcher, and related utilities. This includes protonup/tricks";
 
-	};
+  config = lib.mkIf config.gaming.steam.enable {
+    programs.steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+      remotePlay.openFirewall = true;
+    };
 
-	config = {
-		programs.steam = {
-			enable = true;
-			gamescopeSession.enable = true;
-			remotePlay.openFirewall = true;
-		};
+    environment = {
+      systemPackages = with pkgs; [
+        mangohud
+        protontricks
+        protonup
+      ];
+      sessionVariables = {
+        STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
+      };
+    };
 
-		environment = {
-			systemPackages = with pkgs; [
-				mangohud
-				protontricks
-				protonup
-			];
-			sessionVariables = {
-				STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
-			};
-		};	
-
-		programs.gamemode.enable = true;
-	};
+    programs.gamemode.enable = true;
+  };
 }
