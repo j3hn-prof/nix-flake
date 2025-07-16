0aac327766e3c91a462618f42bc56e7884692a51
j3hn <johnrossi2601@gmail.com>
2025-07-16 16:48:44 -0400

    updated nvidia surface config

--- modules/hardware/sb2.nix
+++ modules/hardware/sb2.nix
@@ -3,6 +3,7 @@
   lib,
   config,
   inputs,
+  user,
   ...
 }:
 let
@@ -24,12 +25,40 @@ in
       "nosgx"
     ];
 
-    services.xserver.videoDrivers = ["nvidia"];
-    hardware.nvidia.open = false;
-
-    environment.systemPackages = with pkgs; [ surface-control ];
+    # nixpkgs.config.nvidia.acceptLicense = true;
+    # # services.xserver.videoDrivers = [ "nvidia" ];
+    # environment.variables = {
+    #   GBM_BACKEND = "nvidia-drm";
+    #   LIBVA_DRIVER_NAME = "nvidia";
+    #   __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    # };
+    hardware = {
+      nvidia = {
+        modesetting.enable = true;
+        powerManagement.enable = true; # Disable if issues with sleep/suspend
+        # package = config.boot.kernelPackages.nvidiaPackages.latest;
+        nvidiaSettings = true;
+        open = false;
+      };
+      # graphics = {
+      #   #driSupport = true;
+      #   enable32Bit = true;
+      #   extraPackages = with pkgs; [
+      #     nvidia-vaapi-driver
+      #     vaapiVdpau
+      #     libvdpau-va-gl
+      #   ];
+      # };
+    };
+    
+    environment.systemPackages = with pkgs; [
+      surface-control
+      # vulkan-loader
+      # vulkan-validation-layers
+      # vulkan-tools
+    ];
     services.udev.packages = with pkgs; [ surface-control];
-    users.groups.surface-control = { };
+    users.users.${user}.extraGroups = ["surface-control"]; # surface-control without sudo
 
     services.thermald.enable = true;
   };
