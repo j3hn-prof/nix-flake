3f7639b36b6f6a7e8677bdb748dc1d35f67bb5cb
j3hn <johnrossi2601@gmail.com>
2025-07-23 16:25:22 -0400

    changes to camera modules on sb2

--- modules/hardware/sb2.nix
+++ modules/hardware/sb2.nix
@@ -19,15 +19,23 @@ in
     };
     boot.extraModprobeConfig = ''
       softdep soc_button_array pre: pinctrl_sunrisepoint
+      options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
     '';
     boot.kernelParams = [
       "nosgx"
     ];
 
+    boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback.out ];
+    boot.kernelModules = [ "v4l2loopback" ];  
+
     services.xserver.videoDrivers = ["nvidia"];
     hardware.nvidia.open = false;
 
-    environment.systemPackages = with pkgs; [ surface-control ];
+    environment.systemPackages = with pkgs; [
+      surface-control
+      libcamera
+      linux-firmware
+    ];
     services.udev.packages = with pkgs; [ surface-control];
     users.groups.surface-control = { };
 
